28ee8f4207381dfbee2c38f89edb81b9983bc047
fang <29148452@qq.com>
2023-10-05 00:10:18 +0800

    add first controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'onboarding/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "onboarding#index"
   get '/up', to: ->(env) { [200, {}, ['ok']] }
 end
